3ad2feddd527becd8f6296d9b2cd8d05f4a8a229
Chet  Chopra <chopra.chet@gmail.com>
2019-05-15 22:53:13 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    return self[self.length - 1] == '.'
   end
 
   def question?
-
+    return self[self.length - 1] == '?'
   end
 
   def exclamation?
-
+    return self[self.length - 1] == '!'
   end
 
   def count_sentences
-
+  self.split(/\.|\?|\!/).delete_if {|sent| sent.size < 2}.count
   end
+  
 end
