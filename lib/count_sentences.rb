75a048b0b55c725dff9c7cc5b1a494abacacaec0
AustinBH <harlowaustin2@gmail.com>
2019-05-15 23:48:42 -0700

    several tests completed, still w.i.p.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,34 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    split_period = self.split(".")
+    split_question = self.split("?")
+    split_exclaim = self.split("!")
+    total = 0
   end
+
 end
